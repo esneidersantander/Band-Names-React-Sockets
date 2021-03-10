8b0694dceecce5f2b62abbd2b40a829b9c240334
LimberthEsneiderSantanderAlcivar <esneider_98@outlook.com>
2021-03-09 21:46:07 -0500

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BandAdd } from "./components/BandAdd";
+import { BandList } from "./components/BandList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div class="alert">
+				<p>
+					Service status:
+					<span className="text-success">Online</span>
+					<span className="text-danger">Offline</span>
+				</p>
+			</div>
+			<h1>BandNames</h1>
+			<hr/>
+			<div class="row">
+				<div class="col-8">
+					<BandList/>
+				</div>
+				<div class="col-4">
+					<BandAdd/>
+				</div>
+			</div>
+        </div>
+    );
 }
 
 export default App;
